3df670cc79545a964e7a138b84eae01a9e6fcdff
Osmar <oecc.06@gmail.com>
2020-03-08 18:39:42 -0600

    Initial commit

---
+++ CustomMemoryCacheExample/Cache/MemoryCache.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Concurrent;
+
+namespace CustomMemoryCacheExample.Cache
+{
+    public class MemoryCache
+    {
+        #region Private Fields
+
+        private static ConcurrentDictionary<string, object> cache;
+
+        #endregion Private Fields
+
+        #region Private Constructors
+
+        static MemoryCache()
+        {
+            cache = new ConcurrentDictionary<string, object>();
+        }
+
+        #endregion Private Constructors
+
+        #region Public Methods
+
+        public static void Add<TValue>(string key, TValue value)
+        {
+            var keyFormatted = ResolveKey<TValue>(key);
+            if (Exist(keyFormatted))
+                return;
+
+            cache.TryAdd(keyFormatted, value);
+        }
+
+        public static void AddOrUpdate<TValue>(string key, TValue value)
+            => cache.AddOrUpdate(ResolveKey<TValue>(key), value, (key, oldValue) => value);
+
+        public static void Delete<TValue>(string key) => cache.TryRemove(ResolveKey<TValue>(key), out _);
+
+        public static bool Exist(string key) => cache.ContainsKey(key);
+
+        public static TValue Get<TValue>(string key)
+            => !cache.TryGetValue(ResolveKey<TValue>(key), out object value) ? default : (TValue)value;
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+        private static string ResolveKey<TValue>(string key)
+            => $"{typeof(TValue)}_{key}";
+
+        #endregion Private Methods
+    }
+}
---
+++ CustomMemoryCacheExample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomMemoryCacheExample.Cache;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CustomMemoryCacheExample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            var  weathers = Summaries.Select((sumary, index) => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = sumary
+            })
+            .ToArray();
+
+            foreach (var item in weathers)
+                MemoryCache.Add(item.Summary, item);            
+
+            return weathers;
+        }
+
+        [HttpGet("{key}")]
+        public WeatherForecast Get(string key) => MemoryCache.Get<WeatherForecast>(key);
+    }
+}
